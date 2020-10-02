41d3708db03e3f3c52001f4546602149ba7c95fa
“Natarkin <snatarkina@mail.ru>
2020-10-02 12:42:05 +0300

    Added function addition and print

--- src/ru/skillbench/matrixworker/IMatrixWorkerImpl.java
+++ src/ru/skillbench/matrixworker/IMatrixWorkerImpl.java
@@ -2,26 +2,41 @@ package ru.skillbench.matrixworker;
 
 import java.util.Arrays;
 
-public class IMatrixWorkerImpl implements IMatrixWorker  {
+public class IMatrixWorkerImpl implements IMatrixWorker {
     @Override
     public void print(double[][] m) {
         for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[i].length; j++) {
-                System.out.print(m[i][j] + "\t");
-            }
-            System.out.println();
+            System.out.println(Arrays.toString(m[i]));
         }
 
     }
 
     @Override
     public boolean haveSameDimension(double[][] m1, double[][] m2) {
-        return false;
+        if (m1.length != m2.length) {
+            return false;
+        }
+        for (int i = 0; i < m1.length; i++) {
+            if (m1[i].length == m2[i].length) {
+                continue;
+            }
+            return false;
+        }
+        return true;
     }
 
     @Override
     public double[][] add(double[][] m1, double[][] m2) {
-        return new double[0][];
+        double[][] sumMatrix = new double[m1.length][];
+        for (int i = 0; i < sumMatrix.length; i++) {
+            sumMatrix[i] = m1[i].clone();
+            for (int j = 0; j < sumMatrix[i].length; j++) {
+                sumMatrix[i][j] += m2[i][j];
+            }
+        }
+
+
+        return sumMatrix;
     }
 
     @Override
